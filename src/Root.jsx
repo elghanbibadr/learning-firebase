3a30cbc1ada4b02f0cc652681acb7468dea665e4
badr ghanbi <bghanbi50@gmail.com>
2023-04-30 14:06:18 +0100

    add the update icon and the jsx for it

--- src/Root.jsx
+++ src/Root.jsx
@@ -2,68 +2,53 @@ import React ,{useState,useEffect} from 'react'
 import { db } from './FireBase'
 import { collection, onSnapshot, query,addDoc, doc,getDocs,deleteDoc } from 'firebase/firestore'
 import trash from "../src/trash-solid.svg";
-
+import update from "../src/pen-to-square-regular.svg";
 const Root = () => {
     const [todos,setTodos]=useState([])
     const [taskTitle,setTaskTitle]=useState("")
     const [loading,setLoading]=useState(false)
     const [deleteClicked,setDeletedClicked]=useState(false)
-
+  const todosCollectionRef=collection(db, "todo");
     const handleTaskChanges=(e) => {
         
-       
-
         setTaskTitle(e.target.value)
     }
+
+    // update todo
+    const updateTodo=async(id,task)=>{
+   
+    }
+
+    const handleUpdate=()=>{}
 
       // delete todo
       const handleDelete = async (e) => {
         setDeletedClicked(true)
         const deletedTodo=e.currentTarget.parentElement;
-        try {
           await deleteDoc(doc(db, "todo", deletedTodo.id));
-          console.log("Document successfully deleted!");
-        } catch (e) {
-          console.error("Error removing document: ", e);
-        }
+        
         setDeletedClicked(false)
       };
 
     // fetch data
     useEffect(() => {
-        const fetchTodos = async () => {
+        const getTodos = async () => {
             setLoading(true)
-          const todosCollection = collection(db, "todo");
-          const todosSnapshot = await getDocs(todosCollection);
-          const todosList = [];
-          todosSnapshot.forEach((doc) => {
-            console.log(doc.data())
-            const todo = doc.data();
-            todosList.push({
-              id: doc.id,
-              task: todo.task,
-            });
-          });
-          setTodos(todosList);
+          const todosSnapshot = await getDocs(todosCollectionRef);
+          setTodos(todosSnapshot.docs.map((doc)=>({...doc.data(),id:doc.id})))
           setLoading(false)
         };
-        fetchTodos();
-      }, [deleteClicked]);
+        getTodos();
+      }, []);
 
     // add data
-    const handleTodoAdded=async(e)=>{
+    const addTodo=async(e)=>{
         e.preventDefault();
         if (taskTitle===""){
             alert('please enter a task title')
              return
         }
-         try {
-      const docRef = await addDoc(collection(db, "todo"), {
-        task: taskTitle,
-      });
-    } catch (e) {
-        console.error("Error adding document: ", e);
-    }
+      const docRef = await addDoc(todosCollectionRef, { task: taskTitle });
     setTaskTitle("");
     }
 
@@ -72,7 +57,7 @@ const Root = () => {
   return (
     <div className='text-center w-[80%] py-10 mx-auto bg-red-600 '>
         <h1 className='text-white mb-3 '>my todos </h1>
-        <form onSubmit={handleTodoAdded} className='flex items-center m-4'>
+        <form onSubmit={addTodo} className='flex items-center m-4'>
             <input className='px-4 py-2 w-full   '  onChange={handleTaskChanges}  value={taskTitle}  type='text' placeholder='add todo' />
             <button className='bg-blue-500 py-2 font-bold px-4'>+ </button>
         </form>
@@ -81,7 +66,10 @@ const Root = () => {
         { !loading &&  todos.length !==0 &&   todos.map(({ task,id})=>{
             return  <div id={id} className='text-black flex justify-between items-center px-4 py-3 m-4 bg-white'>
             <p className='text-black font-semibold'>{task}</p>
-            <img className='h-4 cursor-pointer ' onClick={handleDelete} src={trash}  />
+            <div className='flex items-center'>
+              <img className='h-4 mx-2 cursor-pointer ' onClick={handleDelete} src={trash}  />
+              <img className='h-4 cursor-pointer ' onClick={handleUpdate} src={update}  />
+            </div>
         </div>
         })}
 
